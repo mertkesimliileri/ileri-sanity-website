b3d40aedf5ecaaf567bc968dae699092ea7d9aaa
Mert <mert@ileriisler.com>
2023-04-07 13:36:33 +0300

    i18

--- pages/_app.js
+++ pages/_app.js
@@ -3,3 +3,28 @@ import '../styles/globals.css'
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+const getPathSlugs = () => {
+  // We fetched locales from our API once at build time
+  return ["de", "en", "fr", "ru"].map((locale) => ({
+    params: {
+      locale,
+    },
+  }));
+}
+  
+
+export async function getStaticPaths(...args) {
+  const pathsWithLocale = getPathSlugs();
+  return {
+    paths: pathsWithLocale,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  return {
+    props: {
+      ...params
+    }
+  };
+}
